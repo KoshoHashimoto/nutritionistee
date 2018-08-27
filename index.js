1d8da35c90e8f5bc9e72aa91e8220cf7236d0fba
Kosho Hashimoto <clover71484.msh@gmail.com>
2018-08-27 23:34:39 +0900

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ channelSecret: process.env.LINE_CHANNEL_SECRET
 
 server.listen(process.env.PORT || 3000);
 
+const bot = new line.Client(line_config);
+
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
+res.sendStatus(200);
+
+let events_processed = [];
+
+ req.body.events.forEach((event) => {
+
+        if (event.type == "message" && event.message.type == "text"){
+           
+            if (event.message.text == "‚±‚ñ‚É‚¿‚Í"){
+                
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "‚±‚ê‚Í‚±‚ê‚Í"
+                }));
+            }
+        }
+    });
+
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
+
+
