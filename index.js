9a39aa3a270267e50c1ec4e53626d53def3a097f
KoshoHashimoto <42575655+KoshoHashimoto@users.noreply.github.com>
2018-08-28 20:01:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const LINE_CHANNEL_ACCESS_TOKEN = 'h6q3KVIwGb8XULyKnh/MoRedLHZvuQkmUZMI1k0FshhriZLMLhjZM1OQAuj4ZZ81uVlgMkuEwj1vfPavFop9aGcAgdntr7jh0hx9o9X+lxjBFuIlom7d5JKlXHOnJ/3AG69goXt87/wxrhN1VkmmKgdB04t89/1O/w1cDnyilFU='; 
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
 
-app.use(bodyParser.json());
+const server = require("express")();
+const line = require("@line/bot-sdk"); 
 
 
-var port = (process.env.PORT || 3000);
-var server = app.listen(port, function() {
-    console.log('Node is running on port ' + port);
-});
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, 
+    channelSecret: process.env.LINE_CHANNEL_SECRET 
+};
 
 
-app.post('/webhook', function(req, res, next){
-    res.status(200).end();
-    for (var event of req.body.events){
-        if (event.type == 'message' && event.message.text == 'ハロー'){
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+server.listen(process.env.PORT || 3000);
+
+const bot = new line.Client(line_config);
+
+
+
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+
+    
+    let events_processed = [];
+
+    
+    req.body.events.forEach((event) => {
+        
+        if (event.type == "message" && event.message.type == "text"){
+            
+            if (event.message.text == "こんにちは"){
+                
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
             }
-            var body = {
-                replyToken: event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: 'こんにちはー'
-                }]
-            }
-            var url = 'https://api.line.me/v2/bot/message/reply';
-            request({
-                url: url,
-                method: 'POST',
-                headers: headers,
-                body: body,
-                json: true
-            });
         }
-    }
+    });
+
+    
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
