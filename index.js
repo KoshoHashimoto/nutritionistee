1136db818c40fb263bc01fcc05c71b9b48c8f72c
KoshoHashimoto <42575655+KoshoHashimoto@users.noreply.github.com>
2018-08-29 23:06:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = require("express")();
 const bodyParser = require('body-parser');
 const app = express();
 const request = require('request');
+const mecab = require('mecabaas-client'); 
+const shokuhin = require('shokuhin-db');
+const memory = require('memory-cache');
+const dietitian = require('./dietitian');
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
@@ -33,21 +37,70 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            }
-        }
-    });
+   // Mecabクラウドサービスでメッセージを解析
+            mecab.parse(event.message.text)
+            .then(
+                function(response){
+                var foodList = [];
+                    for (var elem of response){
+                        if (elem.length > 2 && elem[1] == '名詞'){
+                            foodList.push(elem);
+                        }
+                    }
+                    var gotAllNutrition = [];
+                    if (foodList.length > 0){
+                        for (var food of foodList){
+                            gotAllNutrition.push(shokuhin.getNutrition(food[0]));
+                        }
+                        return Promise.all(gotAllNutrition);
+                    }
+                }
+            ).then(
+                function(response){
+// 記憶すべき情報を整理する。
+                    var botMemory = {
+                        confirmedFoodList: [],
+                        toConfirmFoodList: [],
+                        confirmingFood: null
+                    }
+                    for (var nutritionList of responseList){
+                        if (nutritionList.length == 0){
+                            // 少なくとも今回の食品DBでは食品と判断されなかったのでスキップ。
+                            continue;
+                        } else if (nutritionList.length == 1){
+                            // 該当する食品が一つだけ見つかったのでこれで確定した食品リストに入れる。
+                            botMemory.confirmedFoodList.push(nutritionList[0]);
+                        } else if (nutritionList.length > 1){
+                            // 複数の該当食品が見つかったのでユーザーに確認するリストに入れる。
+                            botMemory.toConfirmFoodList.push(nutritionList);
+                        }
+                    }
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
+                    /*
+                     * もし確認事項がなければ、合計カロリーを返信して終了。
+                     * もし確認すべき食品があれば、質問して現在までの状態を記憶に保存。
+                     */
+                    if (botMemory.toConfirmFoodList.length == 0 && botMemory.confirmedFoodList.length > 0){
+                        console.log('Going to reply the total calorie.');
+
+                        // 確認事項はないので、確定した食品のカロリーの合計を返信して終了。
+                        dietitian.replyTotalCalorie(event.replyToken, botMemory.confirmedFoodList);
+
+                    } else if (botMemory.toConfirmFoodList.length > 0){
+                        console.log('Going to ask which food the user had');
+
+                        // どの食品が正しいか確認する。
+                        dietitian.askWhichFood(event.replyToken, botMemory.toConfirmFoodList[0]);
+
+                        // ユーザーに確認している食品は確認中のリストに入れ、確認すべきリストからは削除。
+                        botMemory.confirmingFood = botMemory.toConfirmFoodList[0];
+                        botMemory.toConfirmFoodList.splice(0, 1);
+
+                        // Botの記憶に保存
+                        memory.put(event.source.userId, botMemory);
+                    }
+                }
+            );
         }
-    );
+    }
 });
